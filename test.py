bec7412acb90b7ce1bb06da5c18c9278931a0b86
Vaibhav Kapoor <kapoorvaibhav@seasiaconnect.com>
2025-12-17 16:29:26 +0530

    first push try

--- test.py
+++ test.py
@@ -1,7 +1,68 @@
 #Creating module
-def add(x, y):
-    return (x+y)
-def subtract(x, y):
-    return (x-y)
-print(add(2,3))
-print(subtract(2,3))
+#def add(x, y):
+#    return (x+y)
+#def subtract(x, y):
+#    return (x-y)
+#print(add(2,3))
+#print(subtract(2,3))
+
+ 
+'''import math
+print(math.sqrt(16))'''
+ 
+#python dates
+'''import datetime
+x = datetime.datetime.now()
+print(x)'''
+'''import datetime
+x = datetime.datetime.now()
+print(x.year)
+print(x.strftime("%A"))'''
+ 
+# creating date objects
+'''import datetime
+x = datetime.datetime(2025, 12, 17)
+print(x)'''
+ 
+#strftime() method
+'''import datetime
+x = datetime.datetime(2025, 11, 10)
+print(x.strftime("%B"))'''
+ 
+#Built-in maths functions
+'''x = min(5, 10, 25)
+y = max(5, 10, 25)
+print(x)
+print(y)'''
+ 
+'''x=pow(5,6)
+print(x)'''
+ 
+#math module
+'''import math
+x=math.sqrt(91)
+print(x)'''
+ 
+#Exception handling
+'''try:
+  print(x)
+except:
+  print("An exception occurred")'''
+ 
+#else
+'''try:
+  print("Hello")
+except:
+  print("Something went wrong")
+else:
+  print("Nothing went wrong")'''
+ 
+#Finally
+try:
+  print(x)
+except:
+  print("Something went wrong")
+finally:
+  print("The 'try except' is finished")
+ 
+ 
